c81fc7fedadcf595a00a0ff3ad43581b8454a819
Scar <helsingscar@gmail.com>
2015-11-27 11:42:56 +0200

    Initialize workspace

---
+++ README.md
@@ -0,0 +1,7 @@
+> Интсрукция по установке моего приложения
+
+1. git clone https://github.com/ScarHelsing/mysite.git mysite
+2. cd mysite
+3. bower i
+4. npm i
+5. gulp
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// Stop if there are some problems
+"use strict";
+
+var gulp = require('gulp'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	connect = require('gulp-connect'),
+	livereload = require('gulp-livereload'),
+	sass = require('gulp-sass'),
+	minifyHTML= require('gulp-minify-html'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	spritesmith = require('gulp.spritesmith');
+
+// sprite
+	gulp.task('sprite', function () {
+		var spriteData = gulp.src('images/png/*.png').pipe(spritesmith({
+			imgName: 'sprite.png',
+			cssName: 'sprite.css',
+			padding: 5
+		}));
+		return spriteData.pipe(gulp.dest('img/'));
+	});
+
+// server connect
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true
+	});
+});
+
+// html
+gulp.task('html', function() {
+	return gulp.src('index.html')
+	.pipe(gulp.dest('app/'))
+	.pipe(connect.reload())
+});
+
+// scss
+gulp.task('scss', function () {
+	return gulp.src('scss/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(minifyCSS(''))
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(connect.reload())
+});
+
+// js
+gulp.task('js', function() {
+	return gulp.src('js/*.js')
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js/'))
+		.pipe(connect.reload())
+});
+
+// watch
+gulp.task('watch', function (){
+	gulp.watch('scss/*.scss', ['scss'])
+	gulp.watch('index.html', ['html'])
+	gulp.watch('js/*js', ['js'])
+});
+
+// default
+gulp.task('default', ['connect', 'html', 'scss', 'js', 'watch']);
